b5d91705986cf69fc901d38478cd0ec362ddb4d1
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-07 14:20:37 -0400

    Change Realtor Details

--- config.js
+++ config.js
@@ -2,48 +2,75 @@ const config = {
   property: {
     streetAddress: "11 Glen Abbey Tr",
     city: "Vaughan",
-    cityStateZip: "Vaughan, ON L4H 4K4", // Used in form submission data
-    heroSubtitle: "Epitome of Luxury and Grandeur in the exclusive Kleinburg Area. Register to receive the complete property compendium, virtual tour, and comparables.",
-    backgroundImageUrl: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+    cityStateZip: "Vaughan, ON L4H 4K4",
+    heroSubtitle: "Epitome of Luxury and Grandeur. Register today to receive the exclusive Compendium including Floor Plans, Neighborhood Comps, and more...",
+    backgroundImageUrl: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+    listingUrl: "https://app.realmmlp.ca/s?active=TREB-N11969605&is_map_search=true&ne_lat=43.83479654795312&ne_lng=-79.6384699174286&offset=1&q=treb%2Favailability%3AA%257Cclass%3AFREE%257CsaleOrRent%3ASALE%257CgeoAnd%3AY&sw_lat=43.82978137717714&sw_lng=-79.64690278257142&z=17",
+    virtualTourUrl: "https://www.realtor.ca/l/bQDuo/ka",
+    floorPlanUrl: "https://www.realtor.ca/l/bQDuo/ka",
+    brandedMediaUrl: "https://istreetrealty.ca/",
+    compLinks: [
+      "https://www.realtor.ca/real-estate/27976844/71-nave-street-vaughan-kleinburg-kleinburg",
+      "https://www.realtor.ca/real-estate/28231738/49-appleyard-avenue-vaughan-kleinburg-kleinburg",
+      "https://www.realtor.ca/real-estate/27785830/52-rolling-green-court-vaughan-kleinburg-kleinburg",
+      "https://app.realmmlp.ca/s?%24orderby=price&active=TREB-N11889978&is_map_search=true&ne_lat=43.83801356606088&ne_lng=-79.63496530607858&offset=1&q=treb%2Favailability%3AU%257Cclass%3AFREE%257CsaleOrRent%3ASALE%257CgeoAnd%3AY%257Cstatus%3ASLD%257CsaleDate%3A%253E%253D11%252F01%252F2024&sw_lat=43.82798334378021&sw_lng=-79.65183103636423&z=16",
+      "https://app.realmmlp.ca/s?%24orderby=price&active=TREB-N11245503&is_map_search=true&ne_lat=43.83801356606088&ne_lng=-79.63496530607858&offset=1&q=treb%2Favailability%3AU%257Cclass%3AFREE%257CsaleOrRent%3ASALE%257CgeoAnd%3AY%257Cstatus%3ASLD%257CsaleDate%3A%253E%253D11%252F01%252F2024&sw_lat=43.82798334378021&sw_lng=-79.65183103636423&z=16",
+      "https://app.realmmlp.ca/s?%24orderby=price&active=TREB-N9387032&is_map_search=true&ne_lat=43.83801356606088&ne_lng=-79.63496530607858&offset=1&q=treb%2Favailability%3AU%257Cclass%3AFREE%257CsaleOrRent%3ASALE%257CgeoAnd%3AY%257Cstatus%3ASLD%257CsaleDate%3A%253E%253D11%252F01%252F2024&sw_lat=43.82798334378021&sw_lng=-79.65183103636423&z=16"
+    ],
+    keyFeature: "Epitome of Luxury and Grandeur",
+    featureBenefits: [
+      "Completely finished Luxurious turn-key home including Finished Basement with kitchen and 2 extra bedrooms and Full Landscaping with pool and cabana with outdoor bathroom",
+      "New high-end finishes including Full Kitchen, Herringbone flooring, Appliances (SubZero 72\"  Side-by-side Fridge/Freezer, Wolf 48 Stove, Bosh DW), Quartz countertop, 2 new furnaces and Air Conditioner, and Full Interlock driveway",
+      "3-Car garage, 10 foot ceilings on main, and 9 foot ceilings in basement and 2nd floor"
+    ],
+    beds: "4+2 Beds",
+    baths: "6",
+    price: "$3,148,800",
+    homeType: "Freehold",
+    neighborhoodInfo: "Exclusive Kleinburg Area home surrounded by Luxury homes, schools, parks, and nature.",
+    openHouseDate: "2025-05-10"
   },
   realtor: {
     fullName: "iStreet Realty Group",
-    title: "Brokerage Team",
+    firstName: "iStreet Realty Group",
+    title: "Real Estate Group",
     phone: "416-410-2880",
     email: "rick@istreetrealty.ca",
+    dreNumber: "",
     bio: "At iStreet Realty Group we are built on integrity, insight, and innovation. We combine decades of real estate expertise with cutting-edge technology to deliver unmatched support, transparency, and results for our clients. With over 40 years of hands-on real estate experience and extensive corporate backgrounds, our managing partners provide a balanced and strategic approach to Residential, Commercial, and Investment properties. We prioritize clear communication and use the latest market insights to guide every client toward success—whether buying, selling, or investing. At iStreet Realty Inc., we’re more than real estate experts—we’re your trusted partner in building your future.",
-    photoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/c68f7f74-a1cf-4af6-a787-cf6c973b557f/headshot.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466SHELW62K%2F20250507%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250507T170650Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjELn%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCICV%2BOaqi7RPztQxN5a%2FGejkDLIE3hxa2%2B5iGWHzFqEZNAiEAidVh%2F6%2Bjec4PhZIKX2ChTRekJv%2B3rnOHphQR4vg%2FCssq%2FwMIYhAAGgw2Mzc0MjMxODM4MDUiDA476Nbg%2BGT5dHgmqSrcA10aAxmAeT253CIIPiFG5yASIrwckicz31P6GjplCBkvpFVmdhWqxVS%2BDTmMhMwyVJbSBrZjjmjSSx8kuKkv3QvBlKFnbD3pPohdHohFWcJzAbKsJa3baU2HWtjIU5sk%2FMg96ImmUaa%2F5XjJ7UIh5FCj%2B%2B4jiKLddTwBI4mlYxoEmVC2ZlDyvdkc8Yxz7Wb9wFfT5HKCqRiIY8hFfgjt0Fj%2BQEVmLI3%2B0fPANaYm89ANwqk08QM2MCG1%2F4OG%2BEwCWyuQMpFC7IVFT%2BqZj5ETCokIYe8IOocy4BSPbpY3XEo8i66Wa0ExZAgIglx3TeDifd8LG1MZx%2FbPcCc6urXo84U3jRtVgmzxa5HZ4xUQAK%2BwHbSR4Lu%2F5Vy%2FXvIlSeLJv4lUynUGtUsWA67kVI%2BOmpYS1Cxa9DNqMhu4MUz7yEB%2BLhcGn6%2BGTpQXt%2BKLRxBRY1ugQmTs%2BuStJPQu2ZQ2Kq9XGdLcGV10zfNNE3wC2udbNSq7iMfprH5V8j5OXfUFbNZWW46Nc3HWUdTGcxndJbE%2BrF15eDMNNNxrl69zpSsJjI2evvIfqnINHwugRe5UF9Zaqun4WbFpnW6ir5nVV9qoRFpdwoLEO2xtV2Ue7iB2b8hukVf0fpeFNmnXMO%2Bc7sAGOqUBMcBGoVjWMPXnPEh2WdJxbMcBhGjqOP5dnDAYDKBlKDym8CzxPKjbdu02gbgQ%2FSG1UNowQWeRuqNgPpJ4NcE%2B2K6aBJ7bLGWeKrAc5b97%2Fj5ZChwGzXuzda%2B8xygpNWvAxVYlXb0eHzNgiCgOYSFgDfHu2iSjfXDjTJ3GAavRSSoP2fUdX9qi4btuZtUJKW8TYOmprId%2F1gVEiiPiJqLMS1j%2Bnkb%2B&X-Amz-Signature=333c98803bc8c782fbdda1c3837e1d40c35cfe3536b7f5c258cc50475bd5ab1c&X-Amz-SignedHeaders=host&x-id=GetObject",
+    photoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/c68f7f74-a1cf-4af6-a787-cf6c973b557f/headshot.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB4666RG35OUR%2F20250507%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250507T182000Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjELr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJGMEQCIAWzN7B8ejfrkRJaPG7AfPIgcCyaQrbiOcCFx4hBwjRDAiAUcWVeGFAQLtHOrWuEUQHe1DuUM3PYhmbVH7ZzrANAcSr%2FAwhjEAAaDDYzNzQyMzE4MzgwNSIMiLiKFyY6e0I9KJnWKtwD0GvKHKw2Yk029WxRqDv3VweUuy4eHD%2FYk0vOP3SKZy4Qh554xk0wwkEJTDVjQvi7bUka0Gc4Ut5Cc23SmXbWx8i0Fy1NukpTR5KOaVXc0aMr2WZFJTjCC0KaTNiQx%2BvO%2BHzL9EygVPwWeeIWHImacRyLQ3KWJySpFIG2Ntuq3BF3CYApjqvyGZ9Rq%2Ftlt5Wqu6JeDMBrBE8rFl0WN%2BROl%2Fz9sX%2Ba4yFt7J2oiQacq3SpUXVLa8XWMXRp%2Blz5DD1tQ9q5lnQSirHlH%2BeLT4Gx45zhstZ7clCVbE0hmase3OX%2Br1rGuqAPeHdFWokXETbOcDUhUSxidrw%2BbZQp2Okgwq92Y9PiHA295siVjI16FZAxFyEztB318qOAFDQGWNHzBDV0zViGoWAZ1kfx%2F3Jcl3WFW3J%2Fw%2BZmjVs1O6ldl6DRRDUl4%2F3HZJ7C1CzkhskSmkz%2F9j0HWx4l%2BtErF7Pbq29tTFIjYLYCVMUWLxVDqOqjJo%2B9W0%2FgRweWCvzJDuiJWLkmYestI5ms0PqKOkG2f8gm%2FsobfpMejkQBMBlcYO64KKhhkHmfA9%2FeSsS0rJt5%2B6bNvBIGU2V43x0x3uCIE5D52%2F02AN7ACb3W5ExCXnSPDTawQth1azPdGj4wo7nuwAY6pgEKbEJebjnYa%2BgRvu10gbV792G4Ys%2FVLqHG1ThXHJUsPXQStxt2CnbPGvY9kvSC%2FrY1WqAogTB3nQZAzOKNFN0QuCD1DPw33ZZjih3AnjnOYxKPaI9za6kmzMTAY5vV0dWasBAMGAFcOIlELS4oYpKtAlweuKcm5Ps6AVwPhmdG1ux2nM%2FHOQMJqHRYXkUvYD5xj3pDSSuMfmekbc4Uh7AfJaAa5yIp&X-Amz-Signature=3ad6a21f9d6c871e50ae53152960a6f3a5c431cab7a36149c91d62940ea19fc5&X-Amz-SignedHeaders=host&x-id=GetObject",
     social: {
-      facebook: "https://www.facebook.com/iStreetRealty",
+      linkedin: "",
       instagram: "https://www.instagram.com/istreetrealtygroup",
-      website: "https://istreetrealty.ca/"
+      website: "https://istreetrealty.ca/",
+      facebook: "https://www.facebook.com/iStreetRealty"
     }
   },
   brokerage: {
     name: "HomeLife Partners",
-    logoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/2ad1f3a4-7cea-45f4-bef2-b56767bf550e/brokeragelogo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466SHELW62K%2F20250507%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250507T170650Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjELn%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCICV%2BOaqi7RPztQxN5a%2FGejkDLIE3hxa2%2B5iGWHzFqEZNAiEAidVh%2F6%2Bjec4PhZIKX2ChTRekJv%2B3rnOHphQR4vg%2FCssq%2FwMIYhAAGgw2Mzc0MjMxODM4MDUiDA476Nbg%2BGT5dHgmqSrcA10aAxmAeT253CIIPiFG5yASIrwckicz31P6GjplCBkvpFVmdhWqxVS%2BDTmMhMwyVJbSBrZjjmjSSx8kuKkv3QvBlKFnbD3pPohdHohFWcJzAbKsJa3baU2HWtjIU5sk%2FMg96ImmUaa%2F5XjJ7UIh5FCj%2B%2B4jiKLddTwBI4mlYxoEmVC2ZlDyvdkc8Yxz7Wb9wFfT5HKCqRiIY8hFfgjt0Fj%2BQEVmLI3%2B0fPANaYm89ANwqk08QM2MCG1%2F4OG%2BEwCWyuQMpFC7IVFT%2BqZj5ETCokIYe8IOocy4BSPbpY3XEo8i66Wa0ExZAgIglx3TeDifd8LG1MZx%2FbPcCc6urXo84U3jRtVgmzxa5HZ4xUQAK%2BwHbSR4Lu%2F5Vy%2FXvIlSeLJv4lUynUGtUsWA67kVI%2BOmpYS1Cxa9DNqMhu4MUz7yEB%2BLhcGn6%2BGTpQXt%2BKLRxBRY1ugQmTs%2BuStJPQu2ZQ2Kq9XGdLcGV10zfNNE3wC2udbNSq7iMfprH5V8j5OXfUFbNZWW46Nc3HWUdTGcxndJbE%2BrF15eDMNNNxrl69zpSsJjI2evvIfqnINHwugRe5UF9Zaqun4WbFpnW6ir5nVV9qoRFpdwoLEO2xtV2Ue7iB2b8hukVf0fpeFNmnXMO%2Bc7sAGOqUBMcBGoVjWMPXnPEh2WdJxbMcBhGjqOP5dnDAYDKBlKDym8CzxPKjbdu02gbgQ%2FSG1UNowQWeRuqNgPpJ4NcE%2B2K6aBJ7bLGWeKrAc5b97%2Fj5ZChwGzXuzda%2B8xygpNWvAxVYlXb0eHzNgiCgOYSFgDfHu2iSjfXDjTJ3GAavRSSoP2fUdX9qi4btuZtUJKW8TYOmprId%2F1gVEiiPiJqLMS1j%2Bnkb%2B&X-Amz-Signature=e466bae68474221e32325602983600a018965c4df078456a0f297b44b54458c4&X-Amz-SignedHeaders=host&x-id=GetObject",
+    logoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/2ad1f3a4-7cea-45f4-bef2-b56767bf550e/brokeragelogo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB4666RG35OUR%2F20250507%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250507T182000Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjELr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJGMEQCIAWzN7B8ejfrkRJaPG7AfPIgcCyaQrbiOcCFx4hBwjRDAiAUcWVeGFAQLtHOrWuEUQHe1DuUM3PYhmbVH7ZzrANAcSr%2FAwhjEAAaDDYzNzQyMzE4MzgwNSIMiLiKFyY6e0I9KJnWKtwD0GvKHKw2Yk029WxRqDv3VweUuy4eHD%2FYk0vOP3SKZy4Qh554xk0wwkEJTDVjQvi7bUka0Gc4Ut5Cc23SmXbWx8i0Fy1NukpTR5KOaVXc0aMr2WZFJTjCC0KaTNiQx%2BvO%2BHzL9EygVPwWeeIWHImacRyLQ3KWJySpFIG2Ntuq3BF3CYApjqvyGZ9Rq%2Ftlt5Wqu6JeDMBrBE8rFl0WN%2BROl%2Fz9sX%2Ba4yFt7J2oiQacq3SpUXVLa8XWMXRp%2Blz5DD1tQ9q5lnQSirHlH%2BeLT4Gx45zhstZ7clCVbE0hmase3OX%2Br1rGuqAPeHdFWokXETbOcDUhUSxidrw%2BbZQp2Okgwq92Y9PiHA295siVjI16FZAxFyEztB318qOAFDQGWNHzBDV0zViGoWAZ1kfx%2F3Jcl3WFW3J%2Fw%2BZmjVs1O6ldl6DRRDUl4%2F3HZJ7C1CzkhskSmkz%2F9j0HWx4l%2BtErF7Pbq29tTFIjYLYCVMUWLxVDqOqjJo%2B9W0%2FgRweWCvzJDuiJWLkmYestI5ms0PqKOkG2f8gm%2FsobfpMejkQBMBlcYO64KKhhkHmfA9%2FeSsS0rJt5%2B6bNvBIGU2V43x0x3uCIE5D52%2F02AN7ACb3W5ExCXnSPDTawQth1azPdGj4wo7nuwAY6pgEKbEJebjnYa%2BgRvu10gbV792G4Ys%2FVLqHG1ThXHJUsPXQStxt2CnbPGvY9kvSC%2FrY1WqAogTB3nQZAzOKNFN0QuCD1DPw33ZZjih3AnjnOYxKPaI9za6kmzMTAY5vV0dWasBAMGAFcOIlELS4oYpKtAlweuKcm5Ps6AVwPhmdG1ux2nM%2FHOQMJqHRYXkUvYD5xj3pDSSuMfmekbc4Uh7AfJaAa5yIp&X-Amz-Signature=3691f3b738ad3436ea4b5024eedcca987ae7b0be6ff014172fd29a39a1884fae&X-Amz-SignedHeaders=host&x-id=GetObject",
     address: "3850 Steeles Avenue W. Unit 6, Woodbridge, Ontario"
   },
   openHouse: {
     bundleItems: [
-      { icon: "fas fa-ruler-combined", text: "Architectural Floor Plan: " },
-      { icon: "fas fa-star", text: "Benefit: Completely finished Luxurious turn-key home with finished basement, pool, cabana/bathroom, landscaping" },
-      { icon: "fas fa-check-circle", text: "Benefit: New High-End Finishes: Kitchen, Herringbone flooring, SubZero/Wolf/Bosh appliances, 2 new furnaces & AC, interlock driveway" },
-      { icon: "fas fa-car", text: "3-Car Garage, 10 ft main ceilings, 9 ft 2nd/basement" },
-      { icon: "fas fa-camera-retro", text: "High-Resolution Photo Album and Virtual Tour" },
-      { icon: "fas fa-film", text: "Cinematic Property Tour Video" },
-      { icon: "fas fa-map-location-dot", text: "Kleinburg Neighbourhood Insights & Recent Sales" },
-      { icon: "fas fa-key", text: "Early Access Notifications for New Kleinburg Listings" }
+      { icon: "fas fa-ruler-combined", text: "Detailed Architectural Floor Plans" },
+      { icon: "fas fa-star", text: "List of High-End Finishes & Appliances" },
+      { icon: "fas fa-camera-retro", text: "Private High-Resolution Photo Album" },
+      { icon: "fas fa-film", text: "Exclusive Cinematic Property Tour Video" },
+      { icon: "fas fa-map-location-dot", text: "Neighborhood Insights & Comparables Report" },
+      { icon: "fas fa-file-invoice-dollar", text: "Preliminary Title & Disclosure Summary" },
+      { icon: "fas fa-key", text: "Early Access Notification for Similar Estates" }
     ]
   },
   meta: {
-    pageTitlePrefix: "🏡 iStreet Open House",
+    pageTitlePrefix: "🌟 iStreet Open House",
     navBrandLogoText: "iStreet Realty Group",
-    privacyPolicyLink: "https://istreetrealty.ca/privacy-policy/"
+    privacyPolicyLink: "https://istreetrealty.ca/privacy-policy/",
+    termsLink: "#terms"
   },
   settings: {
     webhookUrl: "https://n8n.salesgenius.co/webhook/openhouse",
-    confettiColors: ['#6187a7', '#f0e9d6', '#ffffff', '#83a363', '#d9ac68']
+    confettiColors: ["#e0c58f", "#fafafa", "#ffffff", "#b89b59", "#1b6ca8"]
   },
   modalQuestions: [
     {
@@ -55,5 +82,6 @@ const config = {
         { value: "gathering_info", text: "Just gathering information at this stage." }
       ]
     }
+    // Add more questions here if needed
   ]
 };
